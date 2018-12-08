c2577c560eb9fa93a347d69e79f379209990e985
Tien Nguyen <tyanzen@gmail.com>
2018-12-08 12:42:39 +0700

    remove testing comment

--- task1/index.js
+++ task1/index.js
@@ -4,17 +4,6 @@ const red = chalk.red;
 const green = chalk.green;
 const blue = chalk.blue;
 
-// const path = require('path');
-// console.log('dirname:', path.dirname(__filename));
-// console.log('basename:', path.basename(__filename));
-// console.log('extname:', path.extname(__filename));
-
-// console.log('resolve', path.resolve('./sub', '../test.txt'));
-
-// console.log('join', path.join('.', 'sub', 'test.txt'));
-// console.log('relative', path.relative(__dirname, '/Users/thanh'));
-// console.log('relative', path.relative('/Users/thanh', __dirname));
-
 var questions = [
   {
     type: 'input',

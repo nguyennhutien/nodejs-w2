5faa66fdbe146549a4c76a821abc4d0713f429a2
Tien Nguyen <tyanzen@gmail.com>
2018-12-09 13:14:00 +0700

    done

--- task3/index.js
+++ task3/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const numeral = require('numeral');
 const { format } = require('date-fns');
 const XLSX = require('xlsx');
 
@@ -10,20 +11,16 @@ fs.readFile(`${path.resolve(__dirname, '../task2/products.json')}`, 'utf8', (err
   }
   const arrProductsList = JSON.parse(data);
   arrProductsList.forEach(e => {
+    e.price = numeral(e.price).format('0,0') + 'VND';
     e.updated = format(new Date(e.dateUpdated), 'MM/dd/yyyy');
     delete e.dateUpdated;
   });
 
-  const jsonProductsList = JSON.stringify(arrProductsList, null, ' ');
-  const arrProductKeys = Object.keys(arrProductsList[0]);
-
-  console.log(jsonProductsList);
-
   // create 'worksheet' object from object
-  const ws = XLSX.utils.json_to_sheet(jsonProductsList, { header: arrProductKeys });
+  const ws = XLSX.utils.json_to_sheet(arrProductsList);
 
   // Optional: config columns width (character length)
-  ws['!cols'] = [{ width: 20 }, { width: 15 }, { width: 20 }, { width: 20 }, { width: 20 }];
+  ws['!cols'] = [{ width: 10 }, { width: 40 }, { width: 20 }, { width: 15 }, { width: 20 }];
 
   // create 'workbook' object (which contains multiple sheet)
   const wb = XLSX.utils.book_new();
@@ -32,5 +29,5 @@ fs.readFile(`${path.resolve(__dirname, '../task2/products.json')}`, 'utf8', (err
   // convert to Microsoft EXCEL workbook and write to a Buffer object
   const buf = XLSX.write(wb, { type: 'buffer', bookType: 'xlsx' });
 
-  fs.writeFileSync('./pub/test-write.xlsx', buf, { encoding: 'binary' });
+  fs.writeFileSync(`${path.resolve(__dirname, 'test-write.xlsx')}`, buf, { encoding: 'binary' });
 });
